b697a966586544f0d0cabe8c276463ad3945dd39
m-eduard <marineduardconstantin2001@gmail.com>
2022-01-06 00:55:41 +0200

    Added yearly update methods for Santa and Child

--- src/entities/Child.java
+++ src/entities/Child.java
@@ -1,60 +1,150 @@
 package entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import enums.AgeCategory;
 import enums.Category;
+import enums.Cities;
 import utils.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Information about a child
+ */
 public class Child {
     private int id;
-    private String firstName;
     private String lastName;
+    private String firstName;
+    private final Cities city;
     private int age;
-    private final List<Double> niceScores;
-    private final List<Category> preferences;
+    private final List<Category> giftsPreferences;
+    private double averageScore;
+    private final List<Double> niceScoreHistory;
+    private double assignedBudget;
+    private final List<Gift> receivedGifts;
+    @JsonIgnore
     private AgeCategory ageCategory;
 
     public Child(final int id, final String firstName, final String lastName,
-                 final int age, final List<Double> niceScores, final List<Category> preferences) {
+                 final Cities city, final int age, final List<Double> niceScores,
+                 final List<Category> preferences) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.city = city;
         this.age = age;
-        this.niceScores = niceScores;
-        this.preferences = preferences;
+        this.niceScoreHistory = niceScores;
+        this.giftsPreferences = preferences;
 
         this.ageCategory = Utils.ageToAgeCategory(age);
-//        this.averageScoreStrategy = AverageScoreFactory.getInstance().createStrategy(ageCategory);
+        this.receivedGifts = new ArrayList<>();
     }
 
+    /**
+     * Copy constructor
+     */
+    public Child(final Child child) {
+        this.id = child.getId();
+        this.firstName = child.getFirstName();
+        this.lastName = child.getLastName();
+        this.city = child.getCity();
+        this.age = child.getAge();
+        this.niceScoreHistory = new ArrayList<>(child.getNiceScoreHistory());
+        this.averageScore = child.getAverageScore();
+        this.assignedBudget = child.getAssignedBudget();
+        this.giftsPreferences = new ArrayList<>(child.getGiftsPreferences());
+        this.ageCategory = child.getAgeCategory();
+        this.receivedGifts = new ArrayList<>(child.getReceivedGifts());
+    }
+
+    /**
+     * Updates the child's age and empties the receivedGifts list,
+     * so a new batch of gifts can be assigned in the new year.
+     */
     public void incrementAge() {
         age += 1;
+        ageCategory = Utils.ageToAgeCategory(age);
+
+        receivedGifts.clear();
     }
 
+    /**
+     * Updates the child's info with new data, gathered from the last year.
+     * @param newScore new year's score
+     * @param newPreferences new year's preferences
+     */
     public void update(Double newScore, List<Category> newPreferences) {
         ageCategory = Utils.ageToAgeCategory(age);
 
-        /**
-         * Insert the new gift preferences into the list starting from
-         * the least important one to the most important, such as in the
-         * end, the list will have the preferences in decreasing order after preference
-         */
+        /* Insert the new gift preferences into the list,
+         * pushing consecutively the least important ones
+         * on the first position, so in the end, the list will
+         * store the preferences decreasingly after priority
+         * */
         for (int i = newPreferences.size() - 1; i >= 0; --i) {
-            /**
-             * Try to remove the preference, if it already exists in the list,
-             * in order to insert it again at the beginning of the list (higher priority)
-             */
-            preferences.remove(newPreferences.get(i));
-            preferences.add(0, newPreferences.get(i));
+            /* Remove the preference, if it already exists,
+             * in order to insert it again, at the beginning
+             * of the list (with higher priority)
+             * */
+            giftsPreferences.remove(newPreferences.get(i));
+            giftsPreferences.add(0, newPreferences.get(i));
         }
 
         if (newScore != null) {
-            niceScores.add(newScore);
+            niceScoreHistory.add(newScore);
         }
     }
 
-    public List<Double> getNiceScores() {
-        return niceScores;
+    public int getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Cities getCity() {
+        return city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Category> getGiftsPreferences() {
+        return giftsPreferences;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public List<Double> getNiceScoreHistory() {
+        return niceScoreHistory;
+    }
+
+    public double getAssignedBudget() {
+        return assignedBudget;
+    }
+
+    public List<Gift> getReceivedGifts() {
+        return receivedGifts;
+    }
+
+    public AgeCategory getAgeCategory() {
+        return ageCategory;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public void setAssignedBudget(double assignedBudget) {
+        this.assignedBudget = assignedBudget;
     }
 }
--- src/entities/Santa.java
+++ src/entities/Santa.java
@@ -1,42 +1,118 @@
 package entities;
 
+import enums.AgeCategory;
 import enums.Category;
-import input.ChildUpdate;
+import factory.AverageScoreFactory;
+import update.ChildUpdate;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Information about Santa
+ */
 public class Santa {
+    /**
+     * Current Santa's budget
+     */
     private double budget;
-    private Map<Category, List<Gift>> availableGifts;
-    private Map<Integer, Child> childrenList;
+    /**
+     * List of gifts stored by their category
+     */
+    private final Map<Category, List<Gift>> availableGifts;
+    /**
+     * Santa's list, represented as a map,
+     * where every kid is mapped to its id
+     */
+    private final Map<Integer, Child> childrenList;
 
-    public Santa(final double budget, final Map<Category, List<Gift>> availableGifts,
+    public Santa(final double budget,
+                 final Map<Category,List<Gift>> availableGifts,
                  final Map<Integer, Child> childrenList) {
         this.budget = budget;
         this.availableGifts = availableGifts;
         this.childrenList = childrenList;
     }
 
+    /**
+     * Updates Santa's resources for the new year
+     * and also updates the info for the children
+     * that are still on its list.
+     * @param newBudget new budget as double
+     * @param newGifts list of gifts
+     * @param newChildren list of children
+     * @param childrenUpdate list of updates for the current year
+     */
     public void update(final double newBudget, final List<Gift> newGifts,
-                       final List<Child> newChildren, List<ChildUpdate> childrenUpdate) {
+                       final List<Child> newChildren,
+                       final List<ChildUpdate> childrenUpdate) {
         this.budget = newBudget;
 
-        /**
-         * Update the children that are on the Santa's list
-         */
-
-
-        /**
-         * Add all the new children in its list (represented as Map)
-         */
-        for (Child child : newChildren) {
-
+        /* Update the children that are on the Santa's list */
+        for (int id : childrenList.keySet()) {
+            childrenList.get(id).incrementAge();
         }
 
-        /**
-         * Remove the children > 18yo
-         */
+        /* Add all the new children in its list (represented as Map) */
+        for (Child child : newChildren) {
+            childrenList.put(child.getId(), child);
+        }
 
+        /* Remove the children > 18yo, by gathering their id-s
+         * and deleting the corresponding entries from the map
+         * */
+        List<Integer> idsToRemove = new ArrayList<>();
+        for (int id : childrenList.keySet()) {
+            if (childrenList.get(id).getAgeCategory() == AgeCategory.YOUNG_ADULT) {
+                idsToRemove.add(id);
+            }
+        }
+        idsToRemove.forEach(childrenList::remove);
+
+        /* Update the info for the children that are still on Santa's list */
+        for (ChildUpdate childUpdate : childrenUpdate) {
+            if (childrenList.containsKey(childUpdate.getId())) {
+                childrenList.get(childUpdate.getId())
+                        .update(childUpdate.getNiceScore(), childUpdate.getGiftsPreference());
+            }
+        }
+    }
+
+    /**
+     * Calculates and assigns the budget to every child
+     */
+    public void setChildrenBudgets() {
+        /* Get the total average score, by summing the
+         * children's scores, sorted by their id-s
+         * */
+        Double totalAverage = 0.0;
+        List<Child> orderedChildren = new ArrayList<>(childrenList.values());
+        orderedChildren.sort(Comparator.comparingInt(Child::getId));
+
+        for (Child child : orderedChildren) {
+            double currentAverageScore = AverageScoreFactory.getInstance()
+                    .createStrategy(child.getAgeCategory())
+                    .getAverageScore(child);
+
+            child.setAverageScore(currentAverageScore);
+            totalAverage += currentAverageScore;
+        }
+
+        /* Use the formula (avgScore * (santaBudget / sumAvgScore))
+         * to get the budget assigned by Santa to every child.
+         * */
+        for (Child child : orderedChildren) {
+            child.setAssignedBudget(child.getAverageScore() * (budget / totalAverage));
+        }
+    }
+
+    public Map<Category, List<Gift>> getAvailableGifts() {
+        return availableGifts;
+    }
+
+    public Map<Integer, Child> getChildrenList() {
+        return childrenList;
     }
 }
